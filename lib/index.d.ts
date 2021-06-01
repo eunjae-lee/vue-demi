b2cce025b266ba2d22a3ce4a6e164e999c058b12
eunjae-lee <karis612@gmail.com>
2021-06-01 11:32:07 +0200

    fix: remove @vue/composition-api support

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,7 +2,6 @@ import * as Vue from 'vue'
 declare const isVue2: boolean
 declare const isVue3: boolean
 declare const Vue2: any
-declare const install: (vue?: any) => void
 /** 
  * @deprecated To avoid bringing in all the tree-shakable modules, this API has been deprecated. Use `Vue2` or named exports instead.
  * Refer to https://github.com/vueuse/vue-demi/issues/41
@@ -18,5 +17,4 @@ export {
   Vue2,
   isVue2,
   isVue3,
-  install,
 }
--- scripts/utils.js
+++ scripts/utils.js
@@ -20,37 +20,10 @@ function copy(name, version, vue) {
   fs.writeFileSync(dest, content, 'utf-8')
 }
 
-function updateVue2API() {
-  const ignoreList = ['version', 'default']
-  const VCA = loadModule('@vue/composition-api')
-  if (!VCA) {
-    console.warn('[vue-demi] Composition API plugin is not found. Please run "npm install @vue/composition-api" to install.')
-    return
-  }
-
-  const exports = Object.keys(VCA).filter(i => !ignoreList.includes(i))
-
-  const esmPath = path.join(dir, 'index.esm.js')
-  let content = fs.readFileSync(esmPath, 'utf-8')
-
-  content = content.replace(
-    /\/\*\*VCA-EXPORTS\*\*\/[\s\S]+\/\*\*VCA-EXPORTS\*\*\//m,
-`/**VCA-EXPORTS**/
-export { ${exports.join(', ')} } from '@vue/composition-api'
-/**VCA-EXPORTS**/`
-    )
-
-  fs.writeFileSync(esmPath, content, 'utf-8')
-  
-}
-
 function switchVersion(version, vue) {
   copy('index.cjs.js', version, vue)
   copy('index.esm.js', version, vue)
   copy('index.d.ts', version, vue)
-
-  if (version === 2)
-    updateVue2API()
 }
 
 
