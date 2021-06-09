d09b748a421edb51a63e8119f969bc72ed4fc8f0
eunjae-lee <karis612@gmail.com>
2021-06-09 11:21:19 +0200

    chore: remove unused part

--- lib/v2/index.d.ts
+++ lib/v2/index.d.ts
@@ -3,19 +3,8 @@ declare const isVue2: boolean
 declare const isVue3: boolean
 declare const Vue2: Vue | undefined
 declare const version: string
-/** 
- * @deprecated To avoid bringing in all the tree-shakable modules, this API has been deprecated. Use `Vue2` or named exports instead.
- * Refer to https://github.com/vueuse/vue-demi/issues/41
- */
-declare const V: Vue
 
-export {
-  V as Vue,
-  Vue2,
-  isVue2,
-  isVue3,
-  version,
-}
+export { Vue2, isVue2, isVue3, version }
 
 /**VCA-EXPORTS**/
 export * from '@vue/composition-api'
